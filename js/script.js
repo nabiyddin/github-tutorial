fd46c2e7e15425409cd1196bb4ba8808f5eae8fc
huawei <example@gmail.com>
2024-07-07 11:28:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// console.log(6 + "9");
+
+//  incr, decr
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(10 % 3);
+
+// console.log(5 * 5 === "25");
+
+// && = va || = yoki
+
+// const isAge = true;
+// const isClose = true;
+// const isChecked = true;
+
+// console.log(isAge && isClose && !isChecked);
+
+// console.log(!isChecked);
+
+console.log(2 + 2 * 2);
